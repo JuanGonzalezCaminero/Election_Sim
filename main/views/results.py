59901ea8a7e2bd844da40082fca00a39b1b33ca3
pabvald <pablo.valdunciel@alumnos.uva.es>
2019-11-18 23:11:25 +0100

    Some folder structure

--- main/views/results.py
+++ main/views/results.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return render(request, 'index/index.html')
 
 def results(request):
     return HttpResponse("Results page")
