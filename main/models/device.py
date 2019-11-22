3d76a4cc0f64327e97340dde402c374a6613f552
adriarr <adrian.arroyocalle@gmail.com>
2019-11-22 20:03:41 +0100

    Session - Create Device

--- main/models/device.py
+++ main/models/device.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Device(models.Model):
-    id = models.IntegerField(primary_key=True)
     first_access = models.DateField()
     last_access = models.DateField()
     default_min_votes_threshold = models.FloatField(default=0.03)
