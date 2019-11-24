b4ffc8683ac739c8399a44855f082909553da076
pabvald <pablo.valdunciel@alumnos.uva.es>
2019-11-24 20:25:13 +0100

    Results CSS style remade

--- main/services/election_service.py
+++ main/services/election_service.py
@@ -4,25 +4,27 @@ import numpy as np
 from .district_service import DistrictService
 
 class ElectionService():
+    districtService = DistrictService()
 
     def getSeatDistribution(self, election_pk):
-        districtService = DistrictService()
+        
         election = get_object_or_404(Election, pk=election_pk)
         districts = District.objects.filter(election=election_pk)
 
         totals_count = {}
         total_special_votes = {"blank":0, "void":0, "blank_percentage":0, "void_percentage":0}
         district_results = []
+
         for d in districts:
-            seat_distribution, special_votes = districtService.getSeatDistribution(d, election.min_votes_threshold)
+            seat_distribution, special_votes = self.districtService.getSeatDistribution(d, election.min_votes_threshold)
 
             district_results.append({"name" : d.name,
                                       "candidatures" : seat_distribution,
                                       "special_votes" : special_votes})
 
 
-            #Add to the global votes for each candidature and to the global counts of
-            #special votes
+            # Add to the global votes for each candidature and to the global counts of
+            # special votes
             for candidature in seat_distribution:
                 try:
                     totals_count[candidature["name"]]["seats"] += candidature["seats"]
@@ -41,13 +43,10 @@ class ElectionService():
         #Compute the total number of votes and assign the percentages
         total_votes = np.sum([totals_count[k]["votes"] for k in totals_count])+total_special_votes["blank"] + total_special_votes["void"]
         for k in totals_count:
-            totals_count[k]["votes_percentage"] = totals_count[k]["votes"]/total_votes
-        total_special_votes["blank_percentage"] = total_special_votes["blank"]/total_votes
-        total_special_votes["void_percentage"] = total_special_votes["void"]/total_votes
+            totals_count[k]["votes_percentage"] = totals_count[k]["votes"]/total_votes*100
 
-
-    @staticmethod
-    def getMockSeatDistribution(election_pk):
+        total_special_votes["blank_percentage"] = total_special_votes["blank"]/total_votes*100
+        total_special_votes["void_percentage"] = total_special_votes["void"]/total_votes*100
 
         results = {
             "election" : {
