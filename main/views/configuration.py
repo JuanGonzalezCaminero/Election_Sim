4eff51cde2ab9ff54aedd0baf050a1eeebb7e6c0
pabvald <pablo.valdunciel@alumnos.uva.es>
2019-11-27 23:03:53 +0100

    Configuration modal becomes a Configuration view

--- main/views/configuration.py
+++ main/views/configuration.py
@@ -1,24 +1,31 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.http import HttpResponse
-from main.services.district_service import DistrictService
 from main.services.device_service import DeviceService
-from main.models import Election, ElectionType, District, Candidature
+from main.models import Device
 from main.forms import ConfigurationForm
 
 class ConfigurationView(TemplateView):
 
-    template_name = "configuration/configuration_modal.html"
+    template_name = "configuration/base_configuration.html"
 
     def get(self, request, *args, **kwargs):
         context = self.get_context_data( *args, **kwargs)
         device_service = DeviceService(request)
-        context["min_votes_threshold"] = device_service.device.get_default_min_votes_threshold()
-        context["form"] = ConfigurationForm()
+        default_min_votes_threshold = device_service.device.get_default_min_votes_threshold()
+        context["form"] = ConfigurationForm(initial={"default_min_votes_threshold" : default_min_votes_threshold})
         return self.render_to_response(context)        
 
 
     def post(self, request, *args, **kwargs):
+        context = self.get_context_data(*args, **kwargs)
+        device = DeviceService(request)
+        form = ConfigurationForm(request.POST)
 
-        return redirect("/")
+        if form.is_valid():
+            #Set new default minimum votes threshold
+
+            return redirect("/")
+        else:          
+            return redirect("configuration/")
 
