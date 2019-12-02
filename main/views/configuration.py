49d7f72a853c76a3ed90cc13ecd852d0bd73d61a
pabvald <pablo.valdunciel@alumnos.uva.es>
2019-12-02 12:38:43 +0100

    django's view creation modeled

--- main/views/configuration.py
+++ main/views/configuration.py
@@ -13,19 +13,20 @@ class ConfigurationView(TemplateView):
         device_service = DeviceService(request)
         default_min_votes_threshold = device_service.get_default_configuration()
         context["form"] = ConfigurationForm(initial={"default_min_votes_threshold" : default_min_votes_threshold})
-        context["saved"] = False
         return self.render_to_response(context)        
 
 
     def post(self, request, *args, **kwargs):
         context = self.get_context_data(*args, **kwargs)
         device_service = DeviceService(request)
-        form = ConfigurationForm(request.POST)
-        saved = False        
-       
-        if form.is_valid():    
-            device_service.modify_default_configuration(form['default_min_votes_threshold'].value())
-            saved = True               
+        form = ConfigurationForm(request.POST)     
+
+        try:
+            if form.is_valid():    
+                device_service.modify_default_configuration(form['default_min_votes_threshold'].value())
+                saved = "success"               
+        except Exception:
+            saved = "error"
 
         return render(request, self.template_name, {'form': form, 'saved' : saved})   
 
